6ea30e3e503e3295433ded3d9d822c5128084bf6
wangye@miot.cn <wangye@miot.cn>
2021-05-22 16:29:09 +0800

    sxdsme

---
+++ app/command/Sxdsme.php
@@ -0,0 +1,29 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\command;
+
+use app\Service\Cm\SxService;
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+
+/**
+ * 爬取神仙代售账号
+ * Class Sxds
+ * @package app\command
+ */
+class Sxdsme extends Command
+{
+    protected function configure()
+    {
+        // 指令配置
+        $this->setName('sxdsme')
+            ->setDescription('the sxdsme command');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        (new SxService())->sxdsme();
+    }
+}
