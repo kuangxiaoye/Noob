c5e21b2fcd861f15ef43f3f25529543dc2f85aec
wangye@miot.cn <wangye@miot.cn>
2021-06-11 11:17:05 +0800

    sxds
    添加空判断

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -8,13 +8,19 @@ class Index
 {
     public function index(){
         $action = Request::param('action');
+        if (empty($action)){
+            return "请选择操作";
+        }
         if ($action  == 'mark'){
             $service = new \app\Service\Cm\SxService();
             $goodsId = Request::param('goosid');
+            if (empty($goodsId)){
+                return "请输入商品号";
+            }
             $service->attentionGoods($goodsId);
             return "关注成功";
         }
 
-        return "请选择操作";
+        return  "干嘛！";
     }
 }
