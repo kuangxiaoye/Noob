75ff2acb53d279747fb8c8ccc1401331dda05851
wangye@miot.cn <wangye@miot.cn>
2021-12-08 00:12:10 +0800

    RSS 订阅

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -2,15 +2,41 @@
 
 namespace app\controller;
 
-use app\Service\Cm\SxService;
-use app\Service\Takeout\BaoWeiService;
-use think\facade\Request;
 
 class Index
 {
+    public function __construct(\think\Request $request)
+    {
+        $this->request = $request;
+    }
+
     public function index()
     {
-        $service = new SxService();
-        $service->reviseGoodsArea();
+        $url =  $this->request->param('url');
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Cookie: SID_navi=001002'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $res = simplexml_load_string($response);
+        foreach ($res->channel->item as $key =>$item){
+            $item->guid=md5($item->title);
+        }
+        $return =$res->asXML();
+
+        print_r($return);
     }
 }
